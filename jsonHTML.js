eca090c5ce977212633144e80648fc99dc8e1b44
trillobite <jparnell0@gmail.com>
2016-10-06 15:22:57 -0700

    improved object removal, see comments within code

--- jsonHTML.js
+++ jsonHTML.js
@@ -26,6 +26,7 @@
 */
 
 //view un-minified micronDB code under the micronDB project repository.
+
 var micronDB=function(){return{db:[],hashTraverse:function(t,r){if(this.db[t]){for(var n=0;n<this.db[t].length;++n)if(this.db[t][n]){var e=r(this.db[t][n]);if(e)return e}return!1}return!1},calcIndex:function(t){if(t){for(var r=0,n=0;n<t.length;++n)r+=t.charCodeAt(n);return r%50}return-1},exists:function(t){var r=this.calcIndex(t);return this.hashTraverse(r,function(r){return r?r.id==t:!1})},makeID:function(){var t=new micronDB,r=0,n=8,e=function(){for(var t="",r="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",e=0;n>e;++e)t+=r.charAt(Math.floor(Math.random()*r.length));return t},i=function(i){var h=function(){for(var h=62*Math.pow(n,2);t.exists(i)&&h>r;)++r,i=e()};return h(),t.exists(i)&&(n=Math.round(n/2+n),h()),i};return i(e())},hash:function(t){if(t.id=void 0===t.id?this.makeID():t.id,this.exists(t.id))return!1;var r=this.calcIndex(t.id);return this.db[r]?(this.db[r][this.db[r].length]=t,t):(this.db[r]=[],this.db[r][this.db[r].length]=t,t)},get:function(t){var r=this.calcIndex(t);return this.hashTraverse(r,function(r){return r.id==t?r:void 0})},remove:function(t){var r=this.calcIndex(t);if(this.db[r])for(var n=0;n<this.db[r].length;++n)if(this.db[r][n]&&this.db[r][n]==this.get(t))return this.db[r].splice(n,1),!this.exists(t)},match:{where:function(t,r){for(var n in r)if("undefined"!=typeof t[n])if("function"==typeof t[n]){if(t[n](r[n])===!0)return r}else if(r[n]==t[n])return r;return!1}},traverse:function(t,r,n){var e=function(t,n){for(var i=[],h=0;h<n.length;++h)if(Array.isArray(n[h])){var f=e(t,n[h]);f.length>0&&(Array.isArray(f)&&f.length<2?i[i.length]=f[0]:i[i.length]=f)}else r(t,n[h])&&(i[i.length]=r(t,n[h]));return i},i=[],h=0,f=function(t,r,n,f){"undefined"!=typeof n[f]&&"number"!=typeof f&&(h?i.length>0&&(i=e(t,i)):i=e(t,r)),++h},a=function(t,r,n,h){if("undefined"!=typeof n[h]&&"number"!=typeof h){var f=e(t,r);if(i.length>0)for(var a=function(t,r){for(var n=0;n<r.length;++n)if(r[n]==t)return!0;return!1},s=0;s<f.length;++s)a(f[s],i)||(i[i.length]=f[s]);else i=e(t,r)}};for(var s in t){"$and"==s&&(h=0);var o={};if(o[s]=t[s],"$or"==s||"$and"==s)for(var u in o[s]){var d={};d[u]=o[s][u],"$or"==s?a(d,n,o[s],u):f(d,n,o[s],u)}else f(o,n,t,s)}return i},insert:function(t){return this.hash(t)},query:function(t){var r;for(var n in t)"undefined"!=typeof this.match[n]&&(r=void 0===r?this.traverse(t[n],this.match[n],this.db):this.traverse(t[n],this.match[n],r));return r}}};
 var arrdb = new micronDB();
 /*var idCache = new micronDB();*/
@@ -166,17 +167,40 @@ var sig = function(typ, prop) {
                 }
                 return this; //everything worked as expected.
             };
-            //remove the object from the DOM.
-            tmp.remove = function() {
-                var divId = this.id;
-                var myNode = document.getElementById(divId);
-                if(myNode) {
-                    while(myNode.firstChild) { //Experimental DOM object removal, jQuery "remove" leaves a temporary memory leak, this is intended to fix that issue.
+            /*
+                Description:
+                    Removes current object from the DOM, and with the arguments, it can also
+                    remove all child objects, and erase from micronDB. If no arguments are given,
+                    the function only detaches object from DOM, but still exists within micronDB.
+                Inputs:
+                    {
+                        db: bool, //to remove object from micronDB.
+                        all: bool, //to remove all child objects contained in the jsonHTML object.
+                    }
+            */
+            tmp.remove = function(param) {
+                var thisObj = this; //easier to call from outside function namespace.
+                var myNode = document.getElementById(thisObj.id);
+                if(myNode) { //is the object still on the DOM?
+                    if(param) { //if param was even used.
+                        if(param.all) { //if all is true, then all child objects must also be removed!
+                            for(var i = 0; i < thisObj.children.length; ++i) { //remove all child objects.
+                                var childObj = thisObj.children[i];
+                                childObj.remove(param); //calls recursively, and passes in the current 'param' value.
+                            }
+                        }
+                        if(param.db) {
+                            arrdb.remove(thisObj.id); //remove from micronDB.
+                        }
+                    }
+                    //remove child objects from the DOM.
+                    while(myNode.firstChild) { //jQuery "remove" leaves a temporary memory leak, this is intended to fix that issue.
                         myNode.removeChild(myNode.firstChild);
                     }
-                    $('#'+divId).remove();                
+
+                    $('#'+thisObj.id).remove(); //remove the parent object from the DOM.
                 } else {
-                    console.log(divId, 'object does not exist, or has already been removed');
+                    console.log('jsonHTML:', thisObj.id, 'does not exist on the DOM. Use arrdb.remove(objID) to remove from micronDB.');
                 }
                 return this;
             };
@@ -191,7 +215,7 @@ var sig = function(typ, prop) {
                     }); //make sure to get from the hash table how the object was originally appended.
 
                 } else {
-                    dfd.reject('Error: Parent of the object not defined. Was it rendered to the DOM yet?');
+                    dfd.reject('jsonHTML: Parent of the object is not defined. Was it rendered to the DOM yet?');
                 }
                 this.state = dfd;
                 return this;
